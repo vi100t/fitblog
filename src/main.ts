7a51fdddda4471133e5d41f9d867f33f4fd44d65
vi100t <dovallevicente@gmail.com>
2024-04-29 01:10:45 -0300

    init commit

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import 'zone.js';
+import { BigCardComponent } from './app/big-card/big-card.component';
+import { SmallCardComponent } from './app/small-card/small-card.component';
+import { MenuComponent } from './app/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
   `,
+  imports: [
+    BigCardComponent,
+    SmallCardComponent,
+    MenuComponent
+  ]
 })
 export class App {
   name = 'Angular';
